bb975ab6c7df4f92f6bf1ee3953946db2ce06afa
Prateek Kovalli <prateekrk1999@gmail.com>
2024-10-29 19:43:38 +0530

    Initial Commit

---
+++ SampleMovieCatalogApp/SampleMovieCatalogAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleMovieCatalogAppApp.swift
+//  SampleMovieCatalogApp
+//
+//  Created by Prateek Kovalli on 29/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleMovieCatalogAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
